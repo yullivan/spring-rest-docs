ca234788750b137450166dce220c83e31c92b94d
Yul Jeon <dalbitstream@gmail.com>
2025-05-15 12:09:14 +0900

    refactor: 테스트 코드 가독성 개선 - MockMvcTester 활용 - ObjectMapper 사용 추상화

--- src/test/java/restdocsexample/BoardRestControllerTest.java
+++ src/test/java/restdocsexample/BoardRestControllerTest.java
@@ -1,22 +1,20 @@
 package restdocsexample;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BoardRestController.class)
 public class BoardRestControllerTest extends RestControllerTest {
@@ -25,15 +23,15 @@ public class BoardRestControllerTest extends RestControllerTest {
     private BoardService boardService;
 
     @Test
-    void findBoards() throws Exception {
-        // given
+    void findBoards() {
         BoardsResponse response = BoardsResponse.from(List.of(new Board(1L, "공지사항")));
-        BDDMockito.given(boardService.findAll()).willReturn(response);
+        given(boardService.findAll()).willReturn(response);
 
-        // when
-        mockMvc.perform(get("/boards"))
-                .andExpect(status().isOk())
-                .andDo(document("board-find-all",
+        assertThat(this.mvc.get()
+                .uri("/boards")
+                .accept(MediaType.APPLICATION_JSON))
+                .hasStatusOk()
+                .apply(document("board-find-all",
                         responseFields(
                                 fieldWithPath("items").type(JsonFieldType.ARRAY)
                                         .description("게시판 목록"),
@@ -41,30 +39,31 @@ public class BoardRestControllerTest extends RestControllerTest {
                                         .description("게시판 아이디"),
                                 fieldWithPath("items[].title").type(JsonFieldType.STRING)
                                         .description("게시판 이름")
-                        )));
+                        )
+                ));
     }
 
     @Test
-    void createBoard() throws Exception {
-        // given
+    void createBoard() {
         BoardResponse response = new BoardResponse(1L, "공지사항");
-        BDDMockito.given(boardService.create(BDDMockito.any())).willReturn(response);
+        given(boardService.create(any())).willReturn(response);
 
-        // when
-        ResultActions 공지사항 = mockMvc.perform(post("/boards")
+        assertThat(this.mvc.post()
+                .uri("/boards")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(new CreateBoardRequest("공지사항"))))
-                .andExpect(status().isOk())
-                .andDo(document("board-create",
+                .content(toJson(new CreateBoardRequest("공지사항"))))
+                .hasStatusOk()
+                .apply(document("board-create",
                         requestFields(
-                          fieldWithPath("title").type(JsonFieldType.STRING)
-                                  .description("게시판 이름")
+                                fieldWithPath("title").type(JsonFieldType.STRING)
+                                        .description("게시판 이름")
                         ),
                         responseFields(
                                 fieldWithPath("id").type(JsonFieldType.NUMBER)
                                         .description("게시판 아이디"),
                                 fieldWithPath("title").type(JsonFieldType.STRING)
                                         .description("게시판 이름")
-                        )));
+                        )
+                ));
     }
 }
--- src/test/java/restdocsexample/RestControllerTest.java
+++ src/test/java/restdocsexample/RestControllerTest.java
@@ -1,46 +1,29 @@
 package restdocsexample;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
 @ActiveProfiles("test")
 @AutoConfigureRestDocs
-@ExtendWith(RestDocumentationExtension.class)
 public abstract class RestControllerTest {
+
+    @Autowired
+    protected MockMvcTester mvc;
 
     @Autowired
     protected ObjectMapper objectMapper;
 
-    protected MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp(
-            WebApplicationContext webApplicationContext,
-            RestDocumentationContextProvider restDocumentationContextProvider
-    ) {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .addFilter(new CharacterEncodingFilter("UTF-8", true))
-                .alwaysDo(MockMvcResultHandlers.print())
-                .apply(
-                        MockMvcRestDocumentation.documentationConfiguration(restDocumentationContextProvider)
-                                .operationPreprocessors()
-                                .withRequestDefaults(Preprocessors.prettyPrint())
-                                .withResponseDefaults(Preprocessors.prettyPrint())
-                )
-                .build();
+    protected String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException(
+                    String.format("%s 객체를 JSON으로 직렬화하는데 실패했습니다. 테스트 데이터의 유효성을 확인하세요.",
+                            object.getClass().getSimpleName()), e);
+        }
     }
 }
